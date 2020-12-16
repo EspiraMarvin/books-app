d2b763f23d22ab7c9c6d92252a331a37bf2b08f1
EspiraMarvin <espiramarvin@gmail.com>
2020-12-16 14:54:47 +0300

    init

---
+++ src/components/BookForm.js
@@ -0,0 +1,25 @@
+import React, { useContext, useState } from 'react';
+import {BookContext} from "../contexts/BookContext";
+
+const BookForm = ({ book }) => {
+    const {addNewBook} = useContext(BookContext);
+    const [title, setTitle] = useState('');
+    const [author, setAuthor] = useState('');
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        addNewBook(title, author);
+        setTitle('');
+        setAuthor('');
+    };
+    return (
+        <form onSubmit={handleSubmit}>
+            <input type="text" placeholder="book title" required value={title}
+            onChange={(e) => setTitle(e.target.value)} />
+            <input type="text" placeholder="book aUthor" required value={author}
+                   onChange={(e) => setAuthor(e.target.value)} />
+            <input type="submit" value="Add Book" />
+        </form>
+    );
+};
+
+export default BookForm;
